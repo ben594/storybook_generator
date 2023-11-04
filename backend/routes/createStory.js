a11505a492fcfcfa74ee6164a47eac5f5bbfec67
ben594 <benli20042013@gmail.com>
2023-11-04 12:27:05 -0400

    Skeleton for create story endpoint, still needs openai prompts

--- backend/routes/createStory.js
+++ backend/routes/createStory.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const { OpenAI } = require('openai');
+const mongoose = require('mongoose');
+const { User, Story } = require('../models/modelSchema');
 
 const openai = new OpenAIApi({
-    apiKey: process.env.OPEN_API_KEY
+    apiKey: process.env.OPEN_API_KEY,
 });
 
 // route: /create-story
@@ -18,10 +20,11 @@ router.post('/', async (req, res) => {
         // create story document in the database
         // get user input from request
         const username = req.body.username;
+        const title = req.body.title;
         const age = req.body.age;
         const mainCharacter = req.body.mainCharacter;
         const keywords = req.body.keywords;
-        const prompt = req.body.prompt;
+        const userPrompt = req.body.prompt;
 
         // check user exists
         const user = await User.findOne({ username: username });
@@ -30,17 +33,53 @@ router.post('/', async (req, res) => {
         }
 
         // get output from chat gpt
+        const chatPrompt = getChatPrompt(age, mainCharacter, keywords, userPrompt);
+        const chatCompletion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: chatPrompt }],
+            model: "gpt-3.5-turbo",
+        });
 
-        // generate new story id
+        // get output from dalle
+        const style = "any style"; // TODO: implement style for dalle images
+        const imagePrompt = getImagePrompt(chatPrompt, style);
+        const image = await openai.createImage({
+            prompt: imagePrompt,
+            n: 1,
+            size: "256x256",
+        });
+        image_url = response.data.data[0].url;
+
+        // TODO: store dalle output in azure
+        hosted_image_url = image_url // TODO: replace this with the actual hosted image url
+
+        // generate new story document and add to database
         const storyID = uuidv4(); 
+        const newStory = new Story({ storyID: storyID, title: title, texts: [chatCompletion], images: [hosted_image_url] });
+        const insertedStory = await newStory.save();
+
+        // link story id to user in the database
+        var userStories = user.storyIDs;
+        userStories.append(hosted_image_url);
+        user.storyIDs = userStories;
+        await user.save();
+
+        // return story object
+        return res.status(201).json(insertedStory);
 
     } catch(err) {
         return res.status(500).send(err.stack);
     }
 });
 
-function getPrompt(age, mainCharacter, keywords, prompt) {
-
+function getChatPrompt(age, mainCharacter, keywords, prompt) {
+    // TODO: fill in more detailed chat prompt
+    const dummyPrompt = "Tell me a story."
+    return dummyPrompt;
 }
 
-module.exports = router;
+function getImagePrompt(description, style) {
+    // TODO: fill in more detailed image prompt
+    const dummyPrompt = "Generate a random image."
+}
+
+module.exports = { router, getChatPrompt, getImagePrompt };
