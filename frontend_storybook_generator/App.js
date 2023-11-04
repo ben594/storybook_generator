b6e6d9fc99e7744c036e56915644c0d8e33aa1a3
William <wc187.duke.edu>
2023-11-04 13:33:56 -0400

    basic commit of simple changes to resolve merge

--- frontend_storybook_generator/App.js
+++ frontend_storybook_generator/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import StorybookApp from './components/StorybookApp';
 
 async function changeScreenOrientation() {
