a0f1526e230dbf6250d95436d1ce38253322f66c
ben594 <benli20042013@gmail.com>
2023-11-04 01:06:28 -0400

    Database models

---
+++ backend/models/modelSchema.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  storyIDs: {
+    type: [
+        {type: Schema.Types.ObjectId, ref: "Story"}
+    ],
+    required: true,
+  }
+});
+
+const StorySchema = new mongoose.Schema({
+    storyID: {
+        type: String,
+        required: true
+    },
+    texts: {
+        type: List[String],
+        required: true
+    },
+    images: {
+        type: List[String],
+        required: true
+    }
+});
+
+const User = mongoose.model("User", UserSchema);
+const Story = mongoose.model("Story", StorySchema);
+
+module.exports = { User, Story };
