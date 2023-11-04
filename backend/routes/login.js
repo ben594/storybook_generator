7bd79f254cf7d93d47743d71100e07799e2fce40
William <wc187.duke.edu>
2023-11-04 02:57:31 -0400

    login route completed

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require("mongoose")
 const router = express.Router()
+const { User, Story } = required('../models/modelSchema')
 
 // route: /login
-
 router.get('/', (req, res) => {
     res.send('Welcome to the login route!');
+});
+
+router.post('/', async (req, res) => {
+    const username = req.body.username
+    const user = await User.findOne({ username: username})
+    if (user === null) {
+        return false
+    }
 });
 
 module.exports = router;
