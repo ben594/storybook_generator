075be026500598c65f45d1a90c0b7eb986c7fa0f
ben594 <benli20042013@gmail.com>
2023-11-04 10:59:27 -0400

    Implemented get story endpoint

--- backend/routes/getStory.js
+++ backend/routes/getStory.js
@@ -1,10 +1,30 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const mongoose = require('mongoose');
+const { User, Story } = require('../models/modelSchema');
 
 // route: /get-story
 
-router.get('/', (req, res) => {
-    res.send('Welcome to the get story route!');
+router.get('/:storyid', async (req, res) => {
+    try {
+        // find story document and return list of texts and image links
+        const { storyID } = req.params;
+        const story = await User.findOne({ storyID: storyID });
+
+        // check if story exists
+        if (story === null) {
+            return res.status(500).send("Story does not exist");
+        }
+
+        const storyData = {
+            texts: story.texts,
+            imageURLs: story.images
+        };
+
+        return res.status(200).json({ story: storyData });
+    } catch (err) {
+        return res.status(500).send(err.stack);
+    }
 });
 
 module.exports = router;
