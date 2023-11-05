2b50207914a670df852093c6d1f4c5728f9d519e
ben594 <benli20042013@gmail.com>
2023-11-04 23:10:27 -0400

    Formatting and buttons for book viewer

--- frontend_storybook_generator/components/BookViewer.js
+++ frontend_storybook_generator/components/BookViewer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
 import PagerView from 'react-native-pager-view';
+import Button from './Button';
 
 const BookViewer = ({ route, navigation }) => {
   const [pages, setPages] = useState([]);
@@ -26,18 +27,21 @@ const BookViewer = ({ route, navigation }) => {
   return (
     <PagerView style={styles.pagerView} initialPage={0} transitionStyle={'curl'} scrollEnabled={true}>
       {pages.map((page) => {
-        console.log("page: ", page);
         return (
           <View key={page.pageNumber}>
-            <Image
-              style={styles.imageView}
-              source={{
-                uri: page.imageURL,
-              }}
-            />
-            <Text style={styles.textView}>
-              {page.text}
-            </Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Button style={{ width: 50 }} onPress={null} text={'Login'}/>
+              <Button onPress={null} text={'Sign Up'}/>
+            </View>
+            <View style={styles.pageView}>
+              <Image
+                style={styles.imageView}
+                source={{ uri: page.imageURL }}
+              />
+              <Text style={styles.textView}>
+                {page.text}
+              </Text>
+            </View>
           </View>
         );
       })}
@@ -50,15 +54,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ead8ca'
   },
+  pageView: {
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   textView: {
-    margin: 50,
-    fontSize: 25,
+    margin: 30,
+    fontSize: 20,
     fontFamily: 'Baskerville'
   },
   imageView: {
-    margin: 50,
-    alignContent: 'center',
-    justifyContent: 'center',
+    margin: "auto",
     width: 256,
     height: 256
   }
