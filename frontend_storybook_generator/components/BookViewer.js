7645d61ec476205bb0d1098b7c16e778044ecf23
ben594 <benli20042013@gmail.com>
2023-11-05 02:12:15 -0500

    Voiceover on page change

--- frontend_storybook_generator/components/BookViewer.js
+++ frontend_storybook_generator/components/BookViewer.js
@@ -3,11 +3,13 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 import PagerView from 'react-native-pager-view';
 import { useFocusEffect } from '@react-navigation/native';
 import * as ScreenOrientation from 'expo-screen-orientation';
+import * as Speech from 'expo-speech';
 
 import Button from './Button';
 
 const BookViewer = ({ route, navigation }) => {
   const [pages, setPages] = useState([]);
+  const [position, setPosition] = useState(0);
 
   useFocusEffect(
     useCallback(() => {
@@ -41,13 +43,30 @@ const BookViewer = ({ route, navigation }) => {
     }
 
     setPages(newPages);
+
+    setTimeout(() => {
+      Speech.speak(newPages[0].text, { rate: 0.8, voice: "com.apple.ttsbundle.Karen-compact" });
+    }, 2000);
   }, []);
 
+  const handlePageScroll = async (e) => {
+    const newPos = e.nativeEvent.position;
+    const offset = e.nativeEvent.offset;
+    console.log("offset: ", offset);
+    if (offset == 0) {
+      console.log("page change detected");
+      const voices = await Speech.getAvailableVoicesAsync();
+      console.log("voices: ", voices);
+      Speech.stop();
+      Speech.speak(pages[newPos].text, { rate: 0.8, voice: "com.apple.ttsbundle.Karen-compact" });
+    }
+  }
+
   return (
-    <PagerView style={styles.pagerView} initialPage={0}>
+    <PagerView style={styles.pagerView} initialPage={0} onPageScroll={(e) => handlePageScroll(e) }>
       {pages.map((page) => {
         return (
-          <View key={page.pageNumber} style={styles.page}>
+          <View key={`${page.pageNumber}`} style={styles.page}>
             <Image
               style={styles.imageView}
               source={{ uri: page.imageURL }}
