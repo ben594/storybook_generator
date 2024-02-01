cd8550b86f8fca3964ae742b921400b72c8b48d1
Jasper Hu <wh169@duke.edu>
2024-02-01 11:23:58 -0500

    add page design

--- frontend_storybook_generator/components/BookViewer.js
+++ frontend_storybook_generator/components/BookViewer.js
@@ -100,11 +100,9 @@ const BookViewer = ({ route, navigation }) => {
 
           return (
             <View key={`${page.pageNumber}`} style={styles.page}>
-              <Image
-                style={styles.backgroundImage}
-                source={require('../assets/image_bg.jpg')}
-              />
               <Image style={styles.imageView} source={{ uri: page.imageURL }} />
+              <Image style={styles.backgroundImage} source={require('../assets/edges2.png')}/>
+
               <View style={isLastPage ? styles.lastPageTextContainer : styles.textContainer}>
                 <Text style={styles.textView}>{page.text}</Text>
                 {/* Render OptionChoices only on the last page */}
@@ -196,12 +194,14 @@ const styles = StyleSheet.create({
   },
   imageView: {
     width: 375,
-    height: 375,
-    flex: 1
+    height: 390,
+    flex: 1,
   },
   backgroundImage: {
-    width: 400,
-    height: 400,
+    resizeMode: 'stretch',
+    position: 'absolute',
+    width: '105%', // or any desired size
+    height: '100%', // or any desired size
     flex: 1
   },
 });
